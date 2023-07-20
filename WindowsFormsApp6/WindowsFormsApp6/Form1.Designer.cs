d5ca4140a3b50e063eeb52b7ec23fee9da4eeb4c
vasilbungurdjievfinki <140083351+vasilbungurdjievfinki@users.noreply.github.com>
2023-07-20 15:37:21 +0200

    Add files via upload

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft PhagsPa", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 105);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft PhagsPa", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(291, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 105);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft PhagsPa", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(371, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 105);
+            this.label3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lucida Handwriting", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(236, 313);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 90);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(666, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 40);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Handwriting", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(147, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 36);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.Yellow;
+            this.progressBar1.Location = new System.Drawing.Point(50, 27);
+            this.progressBar1.Maximum = 150;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(720, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(544, 380);
+            this.progressBar2.Maximum = 500;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(290, 23);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Handwriting", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 29);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Question";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(502, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 40);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Total Time:";
+            this.label7.UseMnemonic = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 474);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public int counter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChangeNumbers();
+            label5.Text = "1";
+            progressBar2.Value = 500;
+            counter = 0;
+            timer1.Start();
+            
+            
+        }
+
+        private void ChangeNumbers()
+        {
+            Random r = new Random();
+            int a, b;
+            char d;
+        
+            int c = r.Next(0,3);
+            if(c==0 || c==1)
+            {
+                if (c == 0)
+                {
+                    a = r.Next(0, 300);
+                    b = r.Next(0, 300);
+                }
+                else
+                {
+                    a = r.Next(0, 100);
+                    b = r.Next(0, 100);
+
+                }
+                if (c==0)
+                {
+                    d = '+';
+                }
+                else 
+                {
+                    d = '-';
+                }
+
+            }
+            else
+            {
+               a = r.Next(0, 20);
+               b = r.Next(0, 20);
+                d = '*';
+            }
+
+            
+            label1.Text = a.ToString();
+            label3.Text = b.ToString();
+            label2.Text = d.ToString();
+        }
+
+        private int getAnswer()
+        {
+            int a = int.Parse(label1.Text);
+            int b = int.Parse(label3.Text);
+            int answer;
+            if (label2.Text == "+")
+            {
+                answer = a + b;
+            }
+            else if (label2.Text == "-")
+            {
+                answer = a - b;
+            }
+            else { answer = a * b; }
+            return answer;
+
+        }
+
+        private bool CheckAnswer(int m)
+        {
+            int a = int.Parse(label1.Text);
+            int b = int.Parse(label3.Text);
+            int answer;
+            if(label2.Text=="+")
+            {
+                answer = a + b;
+            }
+            else if(label2.Text=="-")
+            {
+                answer = a - b;
+            }
+            else { answer = a*b; }
+            
+
+            return (answer == m);  
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool er = int.TryParse(textBox1.Text, out int a);
+            if(!er)
+            {
+                errorProvider1.SetError(textBox1, "Vnesovte nesto sto ne e broj!");
+                return;
+            }
+            bool b = CheckAnswer(int.Parse(textBox1.Text));
+            if (b)
+            {
+                errorProvider1.SetError(textBox1, null);
+                int c = int.Parse(label5.Text);
+                c++;
+                if(c==16)
+                {
+                    GameOver(int.Parse(label4.Text), 3);
+                    return;
+                }
+                label5.Text=c.ToString();
+                progressBar1.Value = c * 10;
+                progressBar2.Value = 500;
+                textBox1.Text = "";
+                
+            }
+            else
+            {
+                GameOver(getAnswer(), 1);
+            }
+            ChangeNumbers();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            if(counter%4==0)
+            {
+                label4.Text=(counter/4).ToString();
+            }
+            int a = int.Parse(label5.Text)+1;
+            int c= progressBar2.Value - a;
+            if(c<=0)
+            {
+                GameOver(getAnswer(), 2);
+                return;
+            }
+            else progressBar2.Value = c;
+        }
+        private void GameOver(int a,int b)
+        {
+            if (b==1)
+            {
+                timer1.Stop();
+                string cc = a.ToString();
+                string message =
+              "Odgovorivte pogresno! Tocniot odgovor bese vsusnost " + cc + ". Dali sakate nova igra?";
+                 string caption = "Greska odgovor";
+                var result = MessageBox.Show(message, caption,
+                                             MessageBoxButtons.YesNo,
+                                             MessageBoxIcon.Question);
+
+             
+                if (result == DialogResult.Yes)
+                {
+                    ChangeNumbers();
+                        timer1.Start();
+                    label5.Text = "1";
+                    progressBar2.Value = 500;
+                    progressBar1.Value = 0;
+                    label4.Text = "0";
+                    counter = 0;
+                    textBox1.Text = "";
+                    
+                    return;
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            if (b == 2)
+            {
+                timer1.Stop();
+                string cc = a.ToString();
+                string message =
+              "Vi istece vremeto. Tocniot odgovor bese vsusnost " + cc + ". Dali sakate nova igra?";
+                string caption = "Isteceno vreme";
+                var result = MessageBox.Show(message, caption,
+                                             MessageBoxButtons.YesNo,
+                                             MessageBoxIcon.Question);
+
+
+                if (result == DialogResult.Yes)
+                {
+                    timer1.Start();
+                    ChangeNumbers();
+                    label5.Text = "1";
+                    progressBar2.Value = 500;
+                    progressBar1.Value = 0;
+                    counter = 0;
+                    label4.Text = "0";
+                    textBox1.Text = "";
+                    return;
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            if (b == 3)
+            {
+                timer1.Stop();
+                string cc = a.ToString();
+                string message =
+              "Vi cestitam. Pobedivte. Vaseto vreme bese " + cc + " sekundi. Dali sakate nova igra?";
+                string caption = "Pobeda";
+                var result = MessageBox.Show(message, caption,
+                                             MessageBoxButtons.YesNo,
+                                             MessageBoxIcon.Information);
+
+
+                if (result == DialogResult.Yes)
+                {
+                    timer1.Start();
+                    ChangeNumbers();
+                    label5.Text = "1";
+                    progressBar2.Value = 500;
+                    progressBar1.Value = 0;
+                    counter = 0;
+                    label4.Text = "0";
+                    textBox1.Text = "";
+                    return;
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
